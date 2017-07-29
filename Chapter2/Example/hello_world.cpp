df3a58ee6143dbcd8c3d44cfa10d69e0ae1b25e3
LunEyx <luneyxlab@gmail.com>
2017-07-30 01:28:33 +0800

    Add Chapter 2

---
+++ Chapter2/Example/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()      // C++ run from main function
+{
+    cout << "Hello, World!\n";      // print "Hello, World!"
+    return 0;
+}
---
+++ Chapter2/Exercise/ex1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()      // C++ run from main function
+{
+    cout << "Hello, programming!\n";      // print "Hello, World!"
+    cout << "Here we go!\n";
+    return 0;
+}
